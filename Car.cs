b76dfc4d9651a0917c0ac7da7ac92ad6ddf1d5b3
Poul Henriksen <pohe@easj.dk>
2019-11-05 09:44:39 +0100

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using GuiExample.Annotations;
+
+namespace GuiExample
+{
+    public class Car:INotifyPropertyChanged
+    {
+        private string _brand;
+
+
+        public string Brand
+        {
+            get { return _brand; }
+            set
+            {
+                _brand = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Car()
+        {
+            _brand = "Toyota";
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
